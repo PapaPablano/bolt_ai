face7b12365f0462d07553b331a277b88e3950c5
PapaPablano <62714634+PapaPablano@users.noreply.github.com>
2025-11-23 22:47:35 -0600

    netlify updates 11-23

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,8 @@ import os
 OHLC_DB_URL = os.getenv("OHLC_DB_URL")
 APP_DB_URL = os.getenv("DATABASE_URL")
 
+
+
 if not OHLC_DB_URL:
     raise RuntimeError("OHLC_DB_URL is required for Timescale queries")
 
